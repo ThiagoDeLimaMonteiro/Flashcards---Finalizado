26a515fd5159fdbcb21fba9dff8798c66c986cad
ThiagoMonteiro <133375957+ThiagoDeLimaMonteiro@users.noreply.github.com>
2024-10-25 15:59:41 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Música',
+    'Qual é o albúm mais vendido do mundo?',
+    'Thriller, Michael Jackson'
+)
+
+criaCartao(
+    'Música',
+    'Qual é o artista com mais ouvinte no Spotify?',
+    'Taylor Swift'
+)
+
+criaCartao(
+    'Música',
+    'Quem é o artista com mais Grammys?',
+    'Beyoncé, com 32 Grammys'
+)
+
+criaCartao(
+    'Música',
+    'Qual o nome verdadeiro da Lana del Ray',
+    'Elizabeth Woolridge Grant'
+)
